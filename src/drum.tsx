6b1764ec145d7ed044623318d91545847ce3fef4
Leidon1 <lejdonshehu123@gmail.com>
2023-10-06 20:55:42 +0200

    Full project

---
+++ src/drum.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { AudioClip } from "./types";
+
+interface DrumProps {
+  audioClip: AudioClip;
+  setDescription: (description: string) => void; // Define the setDescription prop
+}
+
+const Drum = ({ audioClip, setDescription }: DrumProps) => {
+  const playSound = (clip: AudioClip) => {
+    const audioElement = document.getElementById(
+      clip.keyTrigger
+    ) as HTMLAudioElement;
+    if (audioElement) {
+      audioElement.play().catch(console.error);
+      setDescription(clip.description); // Call setDescription here
+    }
+  };
+
+  return (
+    <button
+      className="drum-pad"
+      id={`drum-${audioClip.keyTrigger}`}
+      onClick={() => playSound(audioClip)}
+    >
+      <audio src={audioClip.url} id={audioClip.keyTrigger} className="clip" />
+      {audioClip.keyTrigger}
+    </button>
+  );
+};
+
+export default Drum;
